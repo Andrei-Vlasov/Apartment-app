a59e07ac9ccb06732e1e7832c630df8bb9f513f6
ilkrbk <ilkrbk@gmail.com>
2021-03-10 16:19:04 +0200

    refactoring code js, css

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/main.sass';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,3 +1,3 @@
-export default function Index(){
-    return <h1>Hello, Next.js!</h1>
+export default function Index() {
+    return <h1>Hello, Next.js!</h1>;
 }
