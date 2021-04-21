da33213c4e4bdf243128d6bacd44c94be93a5e2a
genndy007 <kochevgenazp@gmail.com>
2021-04-22 00:42:49 +0300

    Set eslint

--- back-ts/.eslintrc.js
+++ back-ts/.eslintrc.js
@@ -12,13 +12,10 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    // I prefix before interface name
+    '@typescript-eslint/interface-name-prefix': 'always',
   },
 };
