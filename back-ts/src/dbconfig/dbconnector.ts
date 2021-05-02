c8f8ff1153350c632f541347c996731f46e27282
genndy007 <kochevgenazp@gmail.com>
2021-05-03 00:26:35 +0300

    Add basic connection to database

---
+++ back-ts/src/dbconfig/dbconnector.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+const db_user = process.env['DB_USER'];
+const db_password = process.env['DB_PASSWORD'];
+const db_host = process.env['DB_HOST'];
+const db_port = process.env['DB_PORT'];
+const db_name = process.env['DB_NAME'];
+
+const pool = new Pool({
+    connectionString: `postgres://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}`,
+});
+
+export default pool;
