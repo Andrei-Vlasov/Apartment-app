6c5c2856dc3a950ecfd065ba3064a3abaf0250f9
genndy007 <kochevgenazp@gmail.com>
2021-05-04 01:39:42 +0300

    Add testing data for inserting into database

---
+++ back-ts/src/queries_testing/psql_test_queries/insert.sql
@@ -0,0 +1,5 @@
+insert into "User"("Email", "PasswordHash", "DateJoined", "FirstName", "LastName", "PhoneNumber")
+values('t@g.com', 'jnjn', '2016-06-22 19:10:25', 'lol', 'petya', 6789765);
+
+insert into "DealType"("DealName") 
+values ('gypsy');
