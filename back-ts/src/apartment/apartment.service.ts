4b07c18e181998464e19563c8f609d52998e9551
genndy007 <kochevgenazp@gmail.com>
2021-05-04 01:42:57 +0300

    Add apartment adding via backend

--- back-ts/src/apartment/apartment.service.ts
+++ back-ts/src/apartment/apartment.service.ts
@@ -1,13 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import pool from 'src/dbconfig/dbconnector';
+import { CreateApartmentDto } from './dto/create-apartment.dto';
 
 @Injectable()
 export class ApartmentService {
-    async addApartment() {
-        console.log('service_addApartment');
+    async addApartment(dto: CreateApartmentDto) {
+        const {
+            authorId,
+            dateCreated,
+            dealType,
+            locationData,
+            price,
+            title,
+            roomCount,
+            space,
+            livingSpace,
+            height,
+            floor,
+            totalFloors,
+            yearBuilt,
+            condition,
+            heating,
+            walls,
+        } = dto;
+
+        try {
+            const client = await pool.connect();
+            console.log('conn');
+
+            const sql = `insert into "ApartmentAdvert"("AuthorID", "DateCreated", "DealType", "LocationData", "Price", "Title", "RoomCount", "Space", "LivingSpace", "Height", "Floor", "TotalFloors", "Year Built", "Condition", "Heating", "Walls")
+            values (${authorId},'${dateCreated}',${dealType},'${locationData}',${price},'${title}',${roomCount},${space},${livingSpace},${height},${floor},${totalFloors},${yearBuilt},'${condition}','${heating}','${walls}')`;
+
+            const { rows } = await client.query(sql);
+            console.log('sql done!');
+
+            client.release();
+
+            console.log('Insertion done!');
+
+            return rows;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async getAllApartments() {
-        console.log('service_getAllApartments');
+        const client = await pool.connect();
+        const sql = 'select * from "ApartmentAdvert" limit 10';
+
+        const { rows } = await client.query(sql);
+        const apartments = rows;
+
+        client.release();
+
+        return apartments;
     }
 
     async getOneApartment() {
