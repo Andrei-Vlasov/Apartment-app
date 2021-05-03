0c9bd77522496bc3463ac6ac7797576e91c0c530
genndy007 <kochevgenazp@gmail.com>
2021-05-04 01:42:12 +0300

    Add data-to-object of apartment for creation with body of request

---
+++ back-ts/src/apartment/dto/create-apartment.dto.ts
@@ -0,0 +1,18 @@
+export class CreateApartmentDto {
+    readonly authorId;
+    readonly dateCreated;
+    readonly dealType;
+    readonly locationData;
+    readonly price;
+    readonly title;
+    readonly roomCount;
+    readonly space;
+    readonly livingSpace;
+    readonly height;
+    readonly floor;
+    readonly totalFloors;
+    readonly yearBuilt;
+    readonly condition;
+    readonly heating;
+    readonly walls;
+}
