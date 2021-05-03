43671797787c29598b6eff317193457fe1e1c666
genndy007 <kochevgenazp@gmail.com>
2021-05-04 01:40:35 +0300

    Change dbconfig to env variables

--- back-ts/src/dbconfig/dbconnector.ts
+++ back-ts/src/dbconfig/dbconnector.ts
@@ -1,10 +1,13 @@
 import { Pool } from 'pg';
+import { config as configDotenv } from 'dotenv';
 
-const db_user = process.env['DB_USER'];
-const db_password = process.env['DB_PASSWORD'];
-const db_host = process.env['DB_HOST'];
-const db_port = process.env['DB_PORT'];
-const db_name = process.env['DB_NAME'];
+configDotenv();
+
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_host = process.env.DB_HOST;
+const db_port = process.env.DB_PORT;
+const db_name = process.env.DB_NAME;
 
 const pool = new Pool({
     connectionString: `postgres://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}`,
