e7fd39418b86da995db1bea88c14203a78b95fe7
Ilya Korobka <ilkrbk@gmail.com>
2021-05-05 13:09:38 +0300

    start make landing page (main)

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,3 +1,26 @@
+import Head from 'next/head'
+import HeaderLayout from './components/header/header'
+import MainTopBlock from './components/main-top-block/main-top-block'
+import MainViewSelect from './components/main-view-select/main-view-select'
+import MainAbout from './components/main-about/main-about'
+import Cooperation from './components/cooperation/cooperation'
+import Info from './components/main-info/main-info'
+import Footer from './components/footer/footer'
+import Layout from './components/layout'
+
 export default function Index() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+        <Layout>
+            <Head>
+                <title>Appartment</title>
+            </Head>
+            <HeaderLayout></HeaderLayout>
+            <MainTopBlock></MainTopBlock>
+            <MainViewSelect></MainViewSelect>
+            <MainAbout></MainAbout>
+            <Cooperation></Cooperation>
+            <Info></Info>
+            <Footer></Footer>
+        </Layout>
+    );
 }
