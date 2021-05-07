301f30c1811cf5920b670bec9661cbfde279d618
Ilya Korobka <ilkrbk@gmail.com>
2021-05-07 12:01:00 +0300

    change layout / create obj for input and select

--- front/pages/components/create-ad/index.js
+++ front/pages/components/create-ad/index.js
@@ -4,6 +4,55 @@ import Footer from '../footer/footer';
 import Layout from '../layout';
 
 export default function Register() {
+    let inputData = [
+        { placeholder: 'Заголовок' },
+        { placeholder: 'Комнаты' },
+        { placeholder: 'Цена' },
+        { placeholder: 'Год постройки' },
+        { placeholder: 'Цена за кв.' },
+        { placeholder: 'Высота потолка' },
+        { placeholder: 'Этажность дома' },
+        { placeholder: 'Общая площадь' },
+        { placeholder: 'Жилая площадь' },
+        { placeholder: 'Этаж' },
+    ];
+
+    let selectData = [
+        {
+            disabled: 'Год постройки',
+            options: [
+                'АППС',
+                'АППС-люкс',
+                'БСП',
+                'чешский проект',
+                'гостинка',
+                'хрущевка',
+                'дореволюционный',
+                'сталинка',
+            ],
+        },
+        {
+            disabled: 'Стени',
+            options: [
+                'блочные',
+                'панельные',
+                'Кирпичные',
+                'утепленная панель',
+                'монолитно-каркасные',
+            ],
+        },
+        { disabled: 'Отопление', options: ['автономное', 'индивидуальное', 'централизованное'] },
+        {
+            disabled: 'Ремонт',
+            options: [
+                'без ремонта',
+                '&quot;бабушкин ремонт&quot;',
+                'евроремонт',
+                'дизайнерский ремонт',
+            ],
+        },
+    ];
+
     return (
         <Layout>
             <Head>
@@ -13,65 +62,22 @@ export default function Register() {
             <div className="create-ad">
                 <h1 className="create-ad__title">Создание обьявления</h1>
                 <form className="create-ad__form">
-                    <input type="text" placeholder="Заголовок" className="create-ad__input" />
-                    <input type="text" placeholder="Комнаты" className="create-ad__input" />
-                    <input type="text" placeholder="Цена" className="create-ad__input" />
-                    <input type="text" placeholder="Год постройки" className="create-ad__input" />
-                    <input type="text" placeholder="Цена за кв." className="create-ad__input" />
-                    <input type="text" placeholder="Высота потолка" className="create-ad__input" />
-                    <input type="text" placeholder="Этажность дома" className="create-ad__input" />
-                    <input type="text" placeholder="Общая площадь" className="create-ad__input" />
-                    <input type="text" placeholder="Жилая площадь" className="create-ad__input" />
-                    <input type="text" placeholder="Этаж" className="create-ad__input" />
+                    {inputData.map(({ placeholder }) => (
+                        <input type="text" placeholder={placeholder} className="create-ad__input" />
+                    ))}
                     <ul className="create-ad__list">
-                        <li className="create-ad__item">
-                            <select>
-                                <option value disabled selected>
-                                    Год постройки
-                                </option>
-                                <option>АППС</option>
-                                <option>АППС-люкс</option>
-                                <option>БСП</option>
-                                <option>чешский проект</option>
-                                <option>гостинка</option>
-                                <option>хрущевка</option>
-                                <option>дореволюционный</option>
-                                <option>сталинка</option>
-                            </select>
-                        </li>
-                        <li className="create-ad__item">
-                            <select>
-                                <option value disabled selected>
-                                    Стени
-                                </option>
-                                <option>блочные</option>
-                                <option>панельные</option>
-                                <option>Кирпичные</option>
-                                <option>утепленная панель</option>
-                                <option>монолитно-каркасные</option>
-                            </select>
-                        </li>
-                        <li className="create-ad__item">
-                            <select>
-                                <option value disabled selected>
-                                    Отопление
-                                </option>
-                                <option>автономное</option>
-                                <option>индивидуальное</option>
-                                <option>централизованное</option>
-                            </select>
-                        </li>
-                        <li className="create-ad__item">
-                            <select>
-                                <option value disabled selected>
-                                    Ремонт
-                                </option>
-                                <option>без ремонта</option>
-                                <option>&quot;бабушкин ремонт&quot;</option>
-                                <option>евроремонт</option>
-                                <option>дизайнерский ремонт</option>
-                            </select>
-                        </li>
+                        {selectData.map(({ disabled, options }) => (
+                            <li className="create-ad__item">
+                                <select>
+                                    <option value disabled selected>
+                                        {disabled}
+                                    </option>
+                                    {options.map((item) => (
+                                        <option>{item}</option>
+                                    ))}
+                                </select>
+                            </li>
+                        ))}
                     </ul>
                     <button className="create-ad__btn">Создать чертеж</button>
                     <textarea type="text" placeholder="Описание" className="create-ad__textarea" />
