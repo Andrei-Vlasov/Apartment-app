53ff0edffee276d153b5ab091784210b4aae2e4d
Ilya Korobka <ilkrbk@gmail.com>
2021-05-09 12:36:22 +0300

    change layout as obj and made media for main-view-select components

--- front/pages/components/main-view-select/main-view-select.js
+++ front/pages/components/main-view-select/main-view-select.js
@@ -2,71 +2,33 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function MainViewSelect() {
+    let item = [
+        { id: 1, title: 'Новобудови', imgSrc: '/img/new-house-build-img.jpeg', link: '/' },
+        { id: 2, title: 'Оренда квартир', imgSrc: '/img/flat-rent.jpg', link: '/' },
+        { id: 3, title: 'Оренда будинків', imgSrc: '/img/house-rent.jpg', link: '/' },
+        { id: 4, title: 'Продаж квартир', imgSrc: '/img/flat-buy.jpg', link: '/' },
+        { id: 5, title: 'Продаж будинків', imgSrc: '/img/house-buy.jpg', link: '/' },
+    ];
+
     return (
         <>
             <section className="main-view-select">
                 <div className="container">
                     <ul className="main-view-select__list">
-                        <li className="main-view-select__item">
-                            <Link href="/">
-                                <a className="main-view-select__link">
-                                    <h3 className="main-view-select__title">Новобудови</h3>
-                                    <Image
-                                        layout="fill"
-                                        className="main-view-select__img"
-                                        src="/img/new-house-build-img.jpeg"
-                                    />
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="main-view-select__item">
-                            <Link href="/">
-                                <a className="main-view-select__link">
-                                    <h3 className="main-view-select__title">Оренда квартир</h3>
-                                    <Image
-                                        layout="fill"
-                                        className="main-view-select__img"
-                                        src="/img/new-house-build-img.jpeg"
-                                    />
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="main-view-select__item">
-                            <Link href="/">
-                                <a className="main-view-select__link">
-                                    <h3 className="main-view-select__title">Оренда будинків</h3>
-                                    <Image
-                                        layout="fill"
-                                        className="main-view-select__img"
-                                        src="/img/new-house-build-img.jpeg"
-                                    />
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="main-view-select__item">
-                            <Link href="/">
-                                <a className="main-view-select__link">
-                                    <h3 className="main-view-select__title">Продаж квартир</h3>
-                                    <Image
-                                        layout="fill"
-                                        className="main-view-select__img"
-                                        src="/img/new-house-build-img.jpeg"
-                                    />
-                                </a>
-                            </Link>
-                        </li>
-                        <li className="main-view-select__item">
-                            <Link href="/">
-                                <a className="main-view-select__link">
-                                    <h3 className="main-view-select__title">Продаж будинків</h3>
-                                    <Image
-                                        layout="fill"
-                                        className="main-view-select__title__img"
-                                        src="/img/new-house-build-img.jpeg"
-                                    />
-                                </a>
-                            </Link>
-                        </li>
+                        {item.map(({ id, title, imgSrc, link }) => (
+                            <li className="main-view-select__item" key={id}>
+                                <Link href={link}>
+                                    <a className="main-view-select__link">
+                                        <h3 className="main-view-select__title">{title}</h3>
+                                        <Image
+                                            layout="fill"
+                                            className="main-view-select__img"
+                                            src={imgSrc}
+                                        />
+                                    </a>
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </section>
