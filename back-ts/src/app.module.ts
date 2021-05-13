beb57df2767ca68e34cabc51720eff423793e2b7
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-13 06:55:10 +0300

    added typeorm connection

--- back-ts/src/app.module.ts
+++ back-ts/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ApartmentModule } from './apartment/apartment.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [ApartmentModule],
+    imports: [
+        ConfigModule.forRoot({ envFilePath: '.env' }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.MYSQL_DB_HOST,
+            port: Number.parseInt(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [],
+            synchronize: true,
+        }),
+        ApartmentModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
