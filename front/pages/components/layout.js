4be486e6ab819010b5a3e53db0fd1a8461146fe3
Ilya Korobka <ilkrbk@gmail.com>
2021-05-13 14:54:02 +0300

    change layout add footer / header / title is parametr

--- front/pages/components/layout.js
+++ front/pages/components/layout.js
@@ -1,3 +1,18 @@
-export default function Layout({ children }) {
-    return <div className="maincontent">{children}</div>;
+import Head from 'next/head'
+import Footer from './footer/footer'
+import HeaderLayout from './header/header'
+
+export default function Layout({ children, title = 'Appartment' }) {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <div className="maincontent">
+                <HeaderLayout></HeaderLayout>
+                {children}
+                <Footer></Footer>
+            </div>
+        </>
+    );
 }
