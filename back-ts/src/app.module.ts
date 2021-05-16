68e79934878ca78ba9e4b8ca426fb82d5eba46a7
genndy007 <kochevgenazp@gmail.com>
2021-05-16 20:52:14 +0300

    Change env variable name

--- back-ts/src/app.module.ts
+++ back-ts/src/app.module.ts
@@ -11,7 +11,7 @@ import { entities } from './typeorm';
         ConfigModule.forRoot({ envFilePath: '.env' }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: process.env.MYSQL_DB_HOST,
+            host: process.env.DB_HOST,
             port: Number.parseInt(process.env.DB_PORT),
             username: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
