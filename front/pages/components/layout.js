a63d40468e290c53b62de87f50c2e55fbfd94c27
Ilya Korobka <ilkrbk@gmail.com>
2021-05-18 21:26:57 +0300

    include mapbox style

--- front/pages/components/layout.js
+++ front/pages/components/layout.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Footer from './footer/footer'
-import HeaderLayout from './header/header'
+import Head from 'next/head';
+import Footer from './footer/footer';
+import HeaderLayout from './header/header';
 
 export default function Layout({ children, title = 'Appartment' }) {
     return (
         <>
             <Head>
+                <link
+                    href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
+                    rel="stylesheet"
+                />
                 <title>{title}</title>
             </Head>
             <div className="maincontent">
