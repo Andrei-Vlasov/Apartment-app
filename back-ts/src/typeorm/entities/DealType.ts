9f7688c153b86e6bd71c800cf911e5915994d6ae
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-19 17:51:59 +0300

    Deleted Dealtype object

--- back-ts/src/typeorm/entities/DealType.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class DealType {
-    @PrimaryGeneratedColumn()
-    DealID: number;
-    @Column()
-    DealName: string;
-}
