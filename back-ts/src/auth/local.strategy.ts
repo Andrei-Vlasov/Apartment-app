d27f8a0d71c738ebf8ccc1459f04e20828abe49c
genndy007 <kochevgenazp@gmail.com>
2021-05-19 23:29:16 +0300

    Set return type for strategy validation

--- back-ts/src/auth/local.strategy.ts
+++ back-ts/src/auth/local.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
+import { Users } from 'src/typeorm';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -9,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
         super();
     }
 
-    async validate(username: string, password: string) {
+    async validate(username: string, password: string): Promise<Users> {
         const user = await this.authService.validateUser(username, password);
 
         if (!user) throw new UnauthorizedException();
