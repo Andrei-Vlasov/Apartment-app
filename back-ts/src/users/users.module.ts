930e2462dad9e35d72033db4d3c3304e6ba088e1
genndy007 <kochevgenazp@gmail.com>
2021-05-19 22:59:40 +0300

    Add users module

---
+++ back-ts/src/users/users.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/typeorm';
+import { UsersService } from './users.service';
+
+@Module({
+    providers: [UsersService],
+    exports: [UsersService],
+    imports: [TypeOrmModule.forFeature([Users])],
+})
+export class UsersModule {}
