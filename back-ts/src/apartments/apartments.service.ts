f7b9b150a602ba144f023356ec239bcdc71988e6
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-22 01:12:23 +0300

    added basic function to find apartments

--- back-ts/src/apartments/apartments.service.ts
+++ back-ts/src/apartments/apartments.service.ts
@@ -8,4 +8,10 @@ export class ApartmentsService {
     constructor(
         @InjectRepository(Apartments) private readonly apartmentsRepository: Repository<Apartments>
     ) {}
+
+    async findApartmentByID(ApartmentID: number): Promise<Apartments | undefined> {
+        const apartment = await this.apartmentsRepository.findOne({ ApartmentID: ApartmentID });
+
+        return apartment;
+    }
 }
