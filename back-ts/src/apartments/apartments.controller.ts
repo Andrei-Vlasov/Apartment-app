cf790db5cdf370439d77f6e440101e0666d58c7f
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-22 06:23:37 +0300

    added apartment creation

--- back-ts/src/apartments/apartments.controller.ts
+++ back-ts/src/apartments/apartments.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Delete, Param } from '@nestjs/common';
+import { Controller, Body, Post, Get, Delete, Param } from '@nestjs/common';
 import { ApartmentsService } from './apartments.service';
+import { Apartments } from 'src/typeorm';
+import { CreateDto } from './dto/create.dto';
 
 @Controller('apartments')
 export class ApartmentsController {
     constructor(private readonly apartmentsService: ApartmentsService) {}
+    @Post('create')
+    async create(@Body() createDto: CreateDto): Promise<Apartments> {
+        const result = await this.apartmentsService.createApartment(createDto);
 
+        return result;
+    }
     @Get()
     async getAll() {
         const result = await this.apartmentsService.getAllApartments();
--- back-ts/src/apartments/apartments.service.ts
+++ back-ts/src/apartments/apartments.service.ts
@@ -2,12 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Apartments } from 'src/typeorm';
 import { DeleteResult, Repository } from 'typeorm';
+import { CreateDto } from './dto/create.dto';
 
 @Injectable()
 export class ApartmentsService {
     constructor(
         @InjectRepository(Apartments) private readonly apartmentsRepository: Repository<Apartments>
     ) {}
+    async createApartment(createDto: CreateDto): Promise<Apartments | undefined> {
+        const apartment = new Apartments();
+        apartment.AuthorID = createDto.AuthorID;
+        apartment.DateCreated = createDto.DateCreated;
+        apartment.AccommodationType = createDto.AccommodationType;
+        apartment.DealType = createDto.DealType;
+        apartment.GlobalX = createDto.GlobalX;
+        apartment.GlobalY = createDto.GlobalY;
+        apartment.Address = createDto.Address;
+        apartment.LocalityID = createDto.LocalityID;
+        apartment.AreaID = createDto.AreaID;
+        apartment.RegionID = createDto.RegionID;
+        apartment.Price = createDto.Price;
+        apartment.Description = createDto.Description;
+        apartment.ContactInfo = createDto.ContactInfo;
+        apartment.RoomCount = createDto.RoomCount;
+        apartment.Space = createDto.Space;
+        apartment.LivingSpace = createDto.LivingSpace;
+        const added_apartment = await this.apartmentsRepository.save(apartment);
+
+        return added_apartment;
+    }
 
     async getAllApartments(): Promise<Apartments[] | undefined> {
         const apartments = await this.apartmentsRepository.find({});
