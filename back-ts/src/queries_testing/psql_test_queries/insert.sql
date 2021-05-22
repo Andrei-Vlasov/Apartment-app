82bd8841b82c6d40c1af419779a258ba8a168440
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-22 03:02:08 +0300

    Test query for new tables

--- back-ts/src/queries_testing/psql_test_queries/insert.sql
+++ back-ts/src/queries_testing/psql_test_queries/insert.sql
@@ -1,5 +1,4 @@
-insert into "User"("Email", "PasswordHash", "DateJoined", "FirstName", "LastName", "PhoneNumber")
-values('t@g.com', 'jnjn', '2016-06-22 19:10:25', 'lol', 'petya', 6789765);
-
-insert into "DealType"("DealName") 
-values ('gypsy');
+INSERT INTO "apartments"("ApartmentID", "AuthorID", "DateCreated", "AccommodationType", "DealType", "GlobalX",
+                         "GlobalY", "Address", "LocalityID", "AreaID", "RegionID", "Price", "Description", "ContactInfo",
+                         "RoomCount", "Space", "LivingSpace")
+VALUES(0, 0, '2016-06-22 19:10:25', 'flat', 'rent', 1.5, -1.5, 'somewhere', 0, 0, 0, 1200, 'its good', 'call me', 3, 40, 32);
