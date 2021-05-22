3de8752288f5df06fe930ca11f9ccac0b8e69036
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-22 05:44:08 +0300

    added dto for apartment creation

---
+++ back-ts/src/apartments/dto/create.dto.ts
@@ -0,0 +1,25 @@
+export class CreateDto {
+    AuthorID: number;
+    DateCreated: string;
+    AccommodationType: string;
+    DealType: string;
+    GlobalX: number;
+    GlobalY: number;
+    Address: string;
+    LocalityID: number;
+    AreaID: number;
+    RegionID: number;
+    Price: number;
+    Description: string;
+    ContactInfo: string;
+    RoomCount: number;
+    Space: number;
+    LivingSpace: number;
+    Height?: number;
+    Floor?: number;
+    TotalFloors?: number;
+    YearBuilt?: number;
+    Condition?: string;
+    Heating?: string;
+    Walls?: string;
+}
