842c7cbd517d9ebc916c8eeecd1a42225cfd344a
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-26 02:06:42 +0300

    Added default values for numbers

--- back-ts/src/apartments/dto/filter.dto.ts
+++ back-ts/src/apartments/dto/filter.dto.ts
@@ -1,26 +1,30 @@
 export class FilterDto {
     Address?: string;
-    MinPrice?: number;
-    MaxPrice?: number;
-    MinPriceSq?: number;
-    MaxPriceSq?: number;
-    MinSpace?: number;
-    MaxSpace?: number;
-    MinLivingSpace?: number;
-    MaxLivingSpace?: number;
-    MinRoomCount?: number;
-    MaxRoomCount?: number;
-    MinFloor?: number;
-    MaxFloor?: number;
-    MinTotalFloors?: number;
-    MaxTotalFloors?: number;
-    MinHeight?: number;
-    MaxHeight?: number;
-    MinYearBuilt?: number;
-    MaxYearBuilt?: number;
+    MinPrice?: number = 0;
+    MaxPrice?: number = 2147483647;
+    MinPriceSq?: number = 0;
+    MaxPriceSq?: number = 2147483647;
+    MinSpace?: number = 0;
+    MaxSpace?: number = 2147483647;
+    MinLivingSpace?: number = 0;
+    MaxLivingSpace?: number = 2147483647;
+    MinRoomCount?: number = 0;
+    MaxRoomCount?: number = 2147483647;
+    MinFloor?: number = 0;
+    MaxFloor?: number = 2147483647;
+    MinTotalFloors?: number = 0;
+    MaxTotalFloors?: number = 2147483647;
+    MinHeight?: number = 0;
+    MaxHeight?: number = 2147483647;
+    MinYearBuilt?: number = 0;
+    MaxYearBuilt?: number = 2147483647;
     DealTypes?: string[];
     Conditions?: string[];
     WallTypes?: string[];
     HeatingTypes?: string[];
     OrderBy?: string;
+
+    constructor(filterDto) {
+        Object.assign(this, filterDto);
+    }
 }
