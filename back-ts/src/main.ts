4c6b5c68683554fd9c46f1a53e6bedb6df607711
genndy007 <kochevgenazp@gmail.com>
2021-05-29 21:51:26 +0300

    Register session

--- back-ts/src/main.ts
+++ back-ts/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import secureSession from 'fastify-secure-session';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+    app.register(secureSession, {
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+        },
+        secret: process.env.COOKIE_SECRET,
+        salt: process.env.COOKIE_SALT,
+    });
+
     await app.listen(3000);
 }
 bootstrap();
