ec670ad26c6ac0a260192b1c20db0ae0c2f11631
genndy007 <kochevgenazp@gmail.com>
2021-05-30 19:15:33 +0300

    Create session entity

--- back-ts/src/typeorm/index.ts
+++ back-ts/src/typeorm/index.ts
@@ -1,7 +1,8 @@
 import { Apartments } from './entities/Apartment';
 import { Users } from './entities/User';
 import { Images } from './entities/Image';
+import { Session } from './entities/Session';
 
-export const entities = [Apartments, Users, Images];
+export const entities = [Apartments, Users, Images, Session];
 
-export { Apartments, Users, Images };
+export { Apartments, Users, Images, Session };
