56b24ad6591b3c12468a46ea9d00d6745c9f3134
genndy007 <kochevgenazp@gmail.com>
2021-05-30 12:41:49 +0300

    Bring app from fastify to express

--- back-ts/src/main.ts
+++ back-ts/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-    await app.listen(3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app_port = process.env.APP_PORT || 3000;
+    await app.listen(app_port, () => console.log(`Running at port ${app_port}`));
 }
 bootstrap();
