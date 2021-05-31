c9efab875b9cc1ef8a20ecc24aebddf5128d9c9e
Ilya Korobka <ilkrbk@gmail.com>
2021-05-31 10:51:59 +0300

    create request post

--- front/pages/components/register/index.js
+++ front/pages/components/register/index.js
@@ -1,27 +1,40 @@
 import Layout from '../layout';
 import Link from 'next/link';
-import axios from 'axios'
+import axios from 'axios';
 
 export default function Register() {
-    const SubmitClick  = async e => {
+    const SubmitClick = async (e) => {
         e.preventDefault();
-        axios.post('http://localhost:3000/auth/register', {
-            username: e.target.username.value,
-            password: e.target.password.value,
-        })
-        .then(response => {
-            console.log(response);
-        });
-    }
+        axios
+            .post('http://localhost:3000/auth/register', {
+                username: e.target.username.value,
+                password: e.target.password.value,
+            })
+            .then((response) => {
+                console.log(response);
+            });
+    };
 
     return (
         <Layout title="Register Page">
             <div className="login">
                 <h1 className="login__title">Регистрация</h1>
                 <form className="login__form" onSubmit={SubmitClick}>
-                    <input type="text" name="username" placeholder="Имя пользователя" className="login__username" />
-                    <input type="text" name="password" placeholder="Пароль" className="login__password" />
-                    <button type="submit" className="login__btn">Регистрация</button>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Имя пользователя"
+                        className="login__username"
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        placeholder="Пароль"
+                        className="login__password"
+                    />
+                    <button type="submit" className="login__btn">
+                        Регистрация
+                    </button>
                 </form>
                 <Link href="/components/sing-in">
                     <a className="login__link">Войти</a>
