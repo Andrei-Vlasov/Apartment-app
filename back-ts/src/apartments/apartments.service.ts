71a2e4f3e3f06b66b924e540b583a6b34bac2344
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-05-31 18:41:18 +0300

    Added optional numberic parameters

--- back-ts/src/apartments/apartments.service.ts
+++ back-ts/src/apartments/apartments.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Apartments } from 'src/typeorm';
-import { Between, DeleteResult, Repository } from 'typeorm';
+import { Between, DeleteResult, Not, Repository } from 'typeorm';
 import { CreateDto } from './dto/create.dto';
 import { FilterDto } from './dto/filter.dto';
 
@@ -25,12 +25,29 @@ export class ApartmentsService {
 
     async filterApartments(params: FilterDto): Promise<Apartments[] | undefined> {
         const filterDto = new FilterDto({ ...params });
-        const apartments = await this.apartmentsRepository.find({
+        const conditions = {
             Price: Between(filterDto.MinPrice, filterDto.MaxPrice),
-            Space: Between(filterDto.MinSpace, filterDto.MaxLivingSpace),
+            Space: Between(filterDto.MinSpace, filterDto.MaxSpace),
             LivingSpace: Between(filterDto.MinLivingSpace, filterDto.MaxLivingSpace),
             RoomCount: Between(filterDto.MinRoomCount, filterDto.MaxRoomCount),
-        });
+        };
+        if ('MaxFloor' in params || 'MinFloor' in params) {
+            conditions['Floor'] = Between(filterDto.MinFloor, filterDto.MaxFloor);
+        }
+        if ('MaxYearBuilt' in params || 'MinYearBuilt' in params) {
+            conditions['YearBuilt'] = Between(filterDto.MinFloor, filterDto.MaxFloor);
+        }
+        if ('MaxHeight' in params || 'MinHeight' in params) {
+            conditions['Height'] = Between(filterDto.MinHeight, filterDto.MaxHeight);
+        }
+        if ('MaxTotalFloors' in params || 'MinTotalFloors' in params) {
+            conditions['TotalFloors'] = Between(filterDto.MinTotalFloors, filterDto.MaxTotalFloors);
+        }
+        // for (let key in myDictionary) {
+        //     let value = myDictionary[key];
+        //     // Use key and value
+        // }
+        const apartments = await this.apartmentsRepository.find(conditions);
 
         return apartments;
     }
