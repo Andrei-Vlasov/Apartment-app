a28532f683b067580fce4e66c8161f99ad5b4b74
genndy007 <kochevgenazp@gmail.com>
2021-06-03 10:12:45 +0300

    Add guard for apartment creation

--- back-ts/src/apartments/apartments.controller.ts
+++ back-ts/src/apartments/apartments.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Body, Post, Get, Delete, Param, Query } from '@nestjs/common';
+import { Controller, Body, Post, Get, Delete, Param, Query, UseGuards } from '@nestjs/common';
 import { ApartmentsService } from './apartments.service';
 import { Apartments } from 'src/typeorm';
 import { CreateDto } from './dto/create.dto';
 import { FilterDto } from './dto/filter.dto';
+import { AuthenticatedGuard } from '../guards';
 
 @Controller('apartments')
 export class ApartmentsController {
     constructor(private readonly apartmentsService: ApartmentsService) {}
+
     @Post('create')
+    @UseGuards(AuthenticatedGuard)
     async create(@Body() createDto: CreateDto): Promise<Apartments> {
         const result = await this.apartmentsService.createApartment(createDto);
 
