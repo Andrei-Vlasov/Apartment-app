c28e8aa80502ec7097d01abcc8dda3479affc4d8
genndy007 <kochevgenazp@gmail.com>
2021-06-03 07:19:37 +0300

    Remove junk queries folder

--- back-ts/src/queries_testing/psql_test_queries/createdb.sql
+++
@@ -1,6 +0,0 @@
--- at terminal run:
--- dropdb apartmentapp
--- createdb apartmentapp
--- this recreates apartmentapp database
-
-GRANT ALL PRIVILEGES ON DATABASE apartmentapp TO postgres;
--- back-ts/src/queries_testing/psql_test_queries/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "apartments"("ApartmentID", "AuthorID", "DateCreated", "AccommodationType", "DealType", "GlobalX",
-                         "GlobalY", "Address", "LocalityID", "AreaID", "RegionID", "Price", "Description", "ContactInfo",
-                         "RoomCount", "Space", "LivingSpace")
-VALUES(0, 0, '2016-06-22 19:10:25', 'flat', 'rent', 1.5, -1.5, 'somewhere', 0, 0, 0, 1200, 'its good', 'call me', 3, 40, 32);
