93a64030fd14ca168a114f2b18a17371de1731e4
Ilya Korobka <ilkrbk@gmail.com>
2021-06-03 09:32:24 +0300

    change map component useEffect useContext

--- front/pages/map.js
+++ front/pages/map.js
@@ -1,52 +1,60 @@
-export default function Map({ MarkerItem, zoom = 12, center, page }) {
-    const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
-    let map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: center,
-        zoom: zoom,
+import React, { useRef, useEffect, useState, useContext } from 'react';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import Context from './search/context';
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
+
+const Map = ({ MarkerItemP, zoomP = 12, latP, lngP, page }) => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng] = useState(lngP);
+    const [lat] = useState(latP);
+    const [zoom] = useState(zoomP);
+    const [markerItem] = useState(MarkerItemP);
+    const value = useContext(Context);
+
+    useEffect(() => {
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom,
+        });
     });
-    if (page == 'search') {
-        const item = document.querySelector('.search__list-article');
-        const element = document.querySelectorAll('.search__item-article');
-        const ActiveMarker = new mapboxgl.Marker({ color: '#017CEA' });
-        item.addEventListener('mouseover', (e) => {
-            for (let i = 0; i < e.path.length; i++) {
-                for (let j = 0; j < element.length; j++) {
-                    if (e.path[i] == element[j]) {
-                        ActiveMarker.setLngLat([
-                            MarkerItem[parseInt(e.path[i].attributes[1].value) - 1].long,
-                            MarkerItem[parseInt(e.path[i].attributes[1].value) - 1].lat,
-                        ]);
-                        ActiveMarker.addTo(map);
-                    }
+
+    switch (page) {
+        case 'search':
+            useEffect(() => {
+                const activeMarker = new mapboxgl.Marker({ color: '#017CEA' });
+                if (value.lng !== undefined && value.lat !== undefined) {
+                    activeMarker.setLngLat([value.lng, value.lat]);
+                    activeMarker.addTo(map.current);
                 }
-            }
-        });
-        item.addEventListener('mouseout', () => {
-            ActiveMarker.remove(map);
-        });
-        for (let i = 0; i < MarkerItem.length; i++) {
-            let el = document.createElement('div');
-            el.className = 'fas fa-dot-circle';
-            el.style.color = '#017CEA';
-            let marker = new mapboxgl.Marker(el);
-            marker.setLngLat([MarkerItem[i].long, MarkerItem[i].lat]);
-            marker.addTo(map);
-        }
-    } else if (page == 'create') {
-        let marker = new mapboxgl.Marker({ draggable: true, color: '#017CEA' })
-            .setLngLat(center)
-            .addTo(map);
-        marker.on('dragend', () => {
-            let coord = marker.getLngLat();
-            map.setCenter({ lat: coord.lat, lng: coord.lng });
-        });
-    } else {
-        let marker = new mapboxgl.Marker({ color: '#017CEA' });
-        marker.setLngLat([MarkerItem[0].long, MarkerItem[0].lat]);
-        marker.addTo(map);
+                return () => {
+                    activeMarker.remove(map.current);
+                };
+            }, [value.lat, value.lng]);
+            break;
+        case 'create':
+            useEffect(() => {
+                const marker = new mapboxgl.Marker({ draggable: true, color: '#017CEA' })
+                    .setLngLat([lng, lat])
+                    .addTo(map.current);
+                marker.on('dragend', () => {
+                    const coord = marker.getLngLat();
+                    map.current.setCenter({ lat: coord.lat, lng: coord.lng });
+                });
+            });
+            break;
+        case 'post':
+            useEffect(() => {
+                new mapboxgl.Marker({ color: '#017CEA' })
+                    .setLngLat([markerItem.lng, markerItem.lat])
+                    .addTo(map.current);
+            });
+            break;
     }
-    return <></>;
-}
+    return <div id="map" ref={mapContainer} />;
+};
+export default Map;
