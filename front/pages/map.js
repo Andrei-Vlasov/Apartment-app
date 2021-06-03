37df2e7958c299bc3936ee30fedbcc3ab8e67b33
Ilya Korobka <ilkrbk@gmail.com>
2021-06-03 13:11:27 +0300

    change useEffect

--- front/pages/map.js
+++ front/pages/map.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useContext } from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 import Context from './search/context';
 
@@ -7,25 +7,21 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_TOKEN;
 const Map = ({ MarkerItemP, zoomP = 12, latP, lngP, page }) => {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng] = useState(lngP);
-    const [lat] = useState(latP);
-    const [zoom] = useState(zoomP);
-    const [markerItem] = useState(MarkerItemP);
     const value = useContext(Context);
+    const propEffect = page === 'search' ? [page, value.lat, value.lng] : [page];
 
     useEffect(() => {
-        if (map.current) return;
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom,
+            center: [lngP, latP],
+            zoom: zoomP,
         });
-    });
+    }, []);
 
-    switch (page) {
-        case 'search':
-            useEffect(() => {
+    useEffect(() => {
+        switch (page) {
+            case 'search': {
                 const activeMarker = new mapboxgl.Marker({ color: '#017CEA' });
                 if (value.lng !== undefined && value.lat !== undefined) {
                     activeMarker.setLngLat([value.lng, value.lat]);
@@ -34,27 +30,27 @@ const Map = ({ MarkerItemP, zoomP = 12, latP, lngP, page }) => {
                 return () => {
                     activeMarker.remove(map.current);
                 };
-            }, [value.lat, value.lng]);
-            break;
-        case 'create':
-            useEffect(() => {
-                const marker = new mapboxgl.Marker({ draggable: true, color: '#017CEA' })
-                    .setLngLat([lng, lat])
-                    .addTo(map.current);
-                marker.on('dragend', () => {
-                    const coord = marker.getLngLat();
-                    map.current.setCenter({ lat: coord.lat, lng: coord.lng });
-                });
-            });
-            break;
-        case 'post':
-            useEffect(() => {
-                new mapboxgl.Marker({ color: '#017CEA' })
-                    .setLngLat([markerItem.lng, markerItem.lat])
-                    .addTo(map.current);
-            });
-            break;
-    }
+            }
+            case 'create':
+                {
+                    const marker = new mapboxgl.Marker({ draggable: true, color: '#017CEA' })
+                        .setLngLat([lngP, latP])
+                        .addTo(map.current);
+                    marker.on('dragend', () => {
+                        const coord = marker.getLngLat();
+                        map.current.setCenter({ lat: coord.lat, lng: coord.lng });
+                    });
+                }
+                break;
+            case 'post':
+                {
+                    new mapboxgl.Marker({ color: '#017CEA' })
+                        .setLngLat([MarkerItemP.lng, MarkerItemP.lat])
+                        .addTo(map.current);
+                }
+                break;
+        }
+    }, propEffect);
     return <div id="map" ref={mapContainer} />;
 };
 export default Map;
