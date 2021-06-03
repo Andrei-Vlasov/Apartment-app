0a49d67f2aebf4b71aea684114b86cd8492d2692
Ilya Korobka <ilkrbk@gmail.com>
2021-06-03 13:22:50 +0300

    change settings map

--- front/pages/user-ad/index.js
+++ front/pages/user-ad/index.js
@@ -1,93 +1,104 @@
 import Map from '../map';
-import React from 'react';
+import React, {useState} from 'react';
 import SearchItem from '../../components/search-item/index';
 import Layout from '../layout';
+import Context from '../search/context';
 
 export default function Search() {
-    let items = [
-        {
-            id: 1,
-            images: [
-                { id: 1, src: '/img/new-house-build-img.jpeg' },
-                { id: 2, src: '/img/house-buy.jpg' },
-            ],
-            price: '177 000',
-            lat: 50.4378811,
-            long: 30.6050259,
-            pricePerSquare: '177',
-            address: 'ул. Пушкинская 188',
-            mainSquare: '30',
-            liveSquare: '27',
-            rooms: '2',
-            floor: '5',
-            floorMax: '25',
-            repairs: 'евроремонт',
-            heating: 'автономное',
-            year: '1995',
-            wall: 'кирпичные',
-            link: '/post/33',
-            share: '/',
-        },
-        {
-            id: 2,
-            images: [
-                { id: 1, src: '/img/new-house-build-img.jpeg' },
-                { id: 2, src: '/img/new-house-build-img.jpeg' },
-            ],
-            price: '177 000',
-            lat: 50.4740758,
-            long: 30.4419382,
-            pricePerSquare: '177',
-            address: 'ул. Пушкинская 188',
-            mainSquare: '30',
-            liveSquare: '27',
-            rooms: '2',
-            floor: '5',
-            floorMax: '25',
-            repairs: 'евроремонт',
-            heating: 'автономное',
-            year: '1995',
-            wall: 'кирпичные',
-            link: '/',
-            share: '/',
-        },
-        {
-            id: 3,
-            images: [
-                { id: 1, src: '/img/new-house-build-img.jpeg' },
-                { id: 2, src: '/img/new-house-build-img.jpeg' },
-            ],
-            price: '177 000',
-            lat: 50.4510538,
-            long: 30.4598583,
-            pricePerSquare: '177',
-            address: 'ул. Пушкинская 188',
-            mainSquare: '30',
-            liveSquare: '27',
-            rooms: '2',
-            floor: '5',
-            floorMax: '25',
-            repairs: 'евроремонт',
-            heating: 'автономное',
-            year: '1995',
-            wall: 'кирпичные',
-            link: '/',
-            share: '/',
-        },
-    ];
+    const [lat, setLat] = useState();
+    const [lng, setLng] = useState();
+    const value = {
+        setLat,
+        setLng,
+        lat,
+        lng,
+        items: [
+            {
+                id: 1,
+                images: [
+                    { id: 1, src: '/img/new-house-build-img.jpeg' },
+                    { id: 2, src: '/img/house-buy.jpg' },
+                ],
+                price: '177 000',
+                lat: 50.4378811,
+                lng: 30.6050259,
+                pricePerSquare: '177',
+                address: 'Илья',
+                mainSquare: '30',
+                liveSquare: '27',
+                rooms: '2',
+                floor: '5',
+                floorMax: '25',
+                repairs: 'евроремонт',
+                heating: 'автономное',
+                year: '1995',
+                wall: 'кирпичные',
+                link: '/post/33',
+                share: '/',
+            },
+            {
+                id: 2,
+                images: [
+                    { id: 1, src: '/img/new-house-build-img.jpeg' },
+                    { id: 2, src: '/img/new-house-build-img.jpeg' },
+                ],
+                price: '177 000',
+                lat: 50.4740758,
+                lng: 30.4419382,
+                pricePerSquare: '177',
+                address: 'Женя',
+                mainSquare: '30',
+                liveSquare: '27',
+                rooms: '2',
+                floor: '5',
+                floorMax: '25',
+                repairs: 'евроремонт',
+                heating: 'автономное',
+                year: '1995',
+                wall: 'кирпичные',
+                link: '/',
+                share: '/',
+            },
+            {
+                id: 3,
+                images: [
+                    { id: 1, src: '/img/new-house-build-img.jpeg' },
+                    { id: 2, src: '/img/new-house-build-img.jpeg' },
+                ],
+                price: '177 000',
+                lat: 50.4510538,
+                lng: 30.4598583,
+                pricePerSquare: '177',
+                address: 'ул. Пушкинская 188',
+                mainSquare: '30',
+                liveSquare: '27',
+                rooms: '2',
+                floor: '5',
+                floorMax: '25',
+                repairs: 'евроремонт',
+                heating: 'автономное',
+                year: '1995',
+                wall: 'кирпичные',
+                link: '/',
+                share: '/',
+            },
+        ],
+    };
 
     return (
-        <>
+        <Context.Provider value={value}>
             <section className="search-map-wrap">
                 <div className="search">
                     <Layout title="Search Page">
                         <ul className="search__list-article">
-                            {items.map((items) => (
+                            {value.items.map((items) => (
                                 <SearchItem
                                     key={items.id}
                                     itemId={items.id}
                                     images={items.images}
                                     price={items.price}
+                                    lat={items.lat}
+                                    lng={items.lng}
                                     pricePerSquare={items.pricePerSquare}
                                     address={items.address}
                                     mainSquare={items.mainSquare}
@@ -113,8 +124,8 @@ export default function Search() {
                         </ul>
                     </Layout>
                 </div>
-                <Map MarkerItem={items} zoom={8} center={[30.5, 50.5]} page={'search'} />
+                <Map zoom={11} latP={50.5} lngP={30.5} page="search" className="search__map" />
             </section>
-        </>
+            </Context.Provider>
     );
 }
